0c93dad72d93d778e6e6be4b6156a822914cba1b
Steve Flanders <steve@omnition.io>
2024-03-25 11:24:13 +0100

    Update bootstrap.sh

--- demo-app/bootstrap.sh
+++ demo-app/bootstrap.sh
@@ -4,7 +4,7 @@ if [[ ! -d ./tracing-examples ]]; then
   git clone https://github.com/signalfx/tracing-examples.git
 fi
 cp extras/* tracing-examples/signalfx-tracing
-cp -r tracing-examples/signalfx-tracing
+cp -r tracing-examples/opentelemetry-tracing/opentelemetry-java-tracing tracing-examples/signalfx-tracing
 cd tracing-examples/signalfx-tracing
 docker build -f okhttp-and-jedis.Dockerfile -t workshop-app .
 #docker-compose build
